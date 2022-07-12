5149f1f031a74b280f45b62c7a4604ee28102305
manish8826 <107915498+manish8826@users.noreply.github.com>
2022-07-12 12:50:17 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+#Question-"define variables name given Tony Stark, Age =
+# 51, and Tony is genius (with the help of boolean) "
+
+
+
+first_name = "Tony"
+last_name = "Stark"
+
+age = 51
+
+Tony_is_genius = True
+
+# print(first_name,last_name,age,Tony_is_genius)
+print(first_name,last_name)
+print(age)
+print(Tony_is_genius)
